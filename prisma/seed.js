1f86902069fc3d14f113f7f053f68efcac6b3927
sandorsc <sandorsc941018@gmail.com>
2025-08-13 12:45:38 -0400

    seed with multiple objects

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,12 +3,19 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient()
 
 async function seed() {
-  const product = await prisma.product.create({
-    data: {
-      name: 'Creatina',
-      description: 'Creatina monohidratada, aumenta tus reservas de energia y mejora la recuperacion muscular.',
-      price: 18.50,
-    }
+  const product = await prisma.product.createMany({
+    data:  [
+      {
+        name: 'Creatina',
+        description: 'Creatina monohidratada, aumenta tus reservas de energia y mejora la recuperacion muscular',
+        price: 18.50,
+      },
+      {
+        name: 'Whey Protein',
+        description: 'Proteina, mas proteina mas ganancia muscular ;)',
+        price: 25.00,
+      }
+    ]
   })
 }
 
