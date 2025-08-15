b14e2b182710782e4b2a5551da4625e57bcd535e
sandorsc <sandorsc941018@gmail.com>
2025-08-15 16:16:44 -0400

    w-on login

--- app/pages/admin/login.vue
+++ app/pages/admin/login.vue
@@ -4,18 +4,40 @@ definePageMeta({
 })
 
 const form = ref({
-  email: 'sandorsc941018@gmail.com',
-  password: 'Zeref.shopping',
+  email: '',
+  password: '',
+  // email: 'sandorsc941018@gmail.com',
+  // password: 'Zeref.shopping',
 })
 
-const route = useRoute();
-console.log(route)
+const handleLogin = () => {
+  console.log('send login', form.value)
+}
 </script>
 
 <template>
   <div class="login-container">
-    <div class="login-container__card">
-
+    <div class="flex flex-col gap-5 w-full max-w-[500px] h-[400px] bg-gray-200 rounded-md shadow-md p-8">
+      <p class="text-center">Introduzca sus credenciales</p>
+      <form class="flex flex-col gap-3" @submit.prevent="handleLogin">
+        <input
+          id="email"
+          v-model="form.email"
+          type="email"
+          class="default-input"
+          placeholder="Email"
+          autocomplete="email"
+        >
+        <input
+          id="password"
+          v-model="form.password"
+          type="password"
+          class="default-input"
+          placeholder="Contraseña"
+          autocomplete="password"
+        >
+        <button class="default-btn text-white bg-black">Aceptar</button>
+      </form>
     </div>
   </div>
 </template>
@@ -32,7 +54,6 @@ console.log(route)
     width: 100%;
     max-width: 500px;
     height: 500px;
-    //background-color: #1B2430;
 
   }
 }
